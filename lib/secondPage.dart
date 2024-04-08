ca77696df061de3fb45fd2fc8225692d389926ed
arostata@kryteriononline.com <arostata@kryteriononline.com>
2024-04-08 23:25:20 +0800

    Updated code

--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -7,7 +7,7 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Column(
+        title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -16,14 +16,14 @@ class SecondPage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 14,
                 fontWeight: FontWeight.bold,
-              ), // Adjust font size as needed
+              ),
             ),
             Text(
               'April 10, 2024 | 2:30 PM | PHST (UTC +8)',
               style: TextStyle(
                 fontSize: 12,
                 color: Color(0xFF616161),
-              ), // Adjust font size as needed
+              ),
             ),
           ],
         ),
@@ -31,200 +31,159 @@ class SecondPage extends StatelessWidget {
           IconButton(
             onPressed: () {
               // Navigate to profile page
-              Navigator.pushNamed(context, '/profile');
+              // Navigator.pushNamed(context, '/profile');
             },
             icon: Container(
-              padding: const EdgeInsets.all(2),
+              padding: EdgeInsets.all(2),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 border: Border.all(
-                    color: const Color(0xFF675DC2), width: 2), // Add stroke
+                  color: Color(0xFF675DC2),
+                  width: 2,
+                ),
               ),
-              child: const CircleAvatar(
-                backgroundImage: AssetImage(
-                    'lib/assets/avatar_person.png'), // Add your avatar image
+              child: CircleAvatar(
+                backgroundImage: AssetImage('lib/assets/avatar_person.png'),
               ),
             ),
           ),
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0), // Add padding to all sides
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             Expanded(
               child: ListView(
                 children: [
-                  Card(
-                    elevation: 0, // Remove shadow
-                    margin: const EdgeInsets.all(8), // Add margin for spacing
-                    color: const Color.fromRGBO(
-                        103, 93, 194, 0.05), // Background color with 5% opacity
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'The title or label of exam',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          const Text(
-                            'Online Proctor',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF616161), // Change text color
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Handle button press
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(
-                                      0xFF675DC2), // Background color
-                                ),
-                                child: const Text(
-                                  'Review Now',
-                                  style: TextStyle(
-                                      color: Colors.white), // Text color
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                  _buildExamCard(
+                    title: 'The title or label of exam',
+                    subtitle: 'Online Proctor',
+                    dateTime: 'April 10, 2024 | 2:30 PM | PHST (UTC +8)',
+                    onPressed: () {
+                      // Handle button press
+                    },
+                    buttonText: 'Review Now',
+                    isElevatedButton: true,
                   ),
-                  const Divider(
-                    color: Color(0xFFD9D9D9), // Set divider color
-                  ), // Separator Line
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  Divider(
+                    color: Color(0xFFD9D9D9),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     child: Text(
                       'Later',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Card(
-                    elevation: 0, // Remove shadow
-                    margin: const EdgeInsets.all(8), // Add margin for spacing
-                    color: const Color.fromRGBO(
-                        103, 93, 194, 0.05), // Background color with 5% opacity
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'The title or label of exam',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          const Text(
-                            'May 5, 2024 | 4:00 PM | PHST (UTC +8)',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF616161), // Change text color
-                            ),
-                          ),
-                          const Text(
-                            'Online Proctor',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF616161), // Change text color
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  // Handle button press
-                                },
-                                child: const Text(
-                                  'Review',
-                                  style: TextStyle(
-                                    color: Color(0xFF675DC2), // Text color
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  Card(
-                    elevation: 0, // Remove shadow
-                    margin: const EdgeInsets.all(8), // Add margin for spacing
-                    color: const Color.fromRGBO(
-                        103, 93, 194, 0.05), // Background color with 5% opacity
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'The title or label of exam',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          const Text(
-                            'May 5, 2024 | 4:00 PM | PHST (UTC +8)',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF616161), // Change text color
-                            ),
-                          ),
-                          const Text(
-                            'Online Proctor',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF616161), // Change text color
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  // Handle button press
-                                },
-                                child: const Text(
-                                  'Review',
-                                  style: TextStyle(
-                                    color: Color(0xFF675DC2), // Text color
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                  _buildExamCard(
+                    title: 'The title or label of exam',
+                    subtitle: 'May 5, 2024 | 4:00 PM | PHST (UTC +8)',
+                    dateTime: 'Online Proctor',
+                    onPressed: () {
+                      // Handle button press
+                    },
+                    buttonText: 'Review',
+                    isElevatedButton: false,
+                  ),
+                  _buildExamCard(
+                    title: 'The title or label of exam',
+                    subtitle: 'May 5, 2024 | 4:00 PM | PHST (UTC +8)',
+                    dateTime: 'Online Proctor',
+                    onPressed: () {
+                      // Handle button press
+                    },
+                    buttonText: 'Review',
+                    isElevatedButton: false,
                   ),
                   // Add more CardViews as needed
                 ],
               ),
             ),
             SizedBox(
-              // Add SizedBox to ensure proper spacing
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
                   // Handle button press
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF675DC2), // Background color
+                  backgroundColor: Color(0xFF675DC2),
                 ),
-                child: const Text(
+                child: Text(
                   'Register another exam',
-                  style: TextStyle(color: Colors.white), // Text color
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildExamCard({
+    required String title,
+    required String subtitle,
+    required String dateTime,
+    required VoidCallback onPressed,
+    required String buttonText,
+    required bool isElevatedButton,
+  }) {
+    return Card(
+      elevation: 0,
+      margin: EdgeInsets.all(8),
+      color: Color.fromRGBO(103, 93, 194, 0.05),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              dateTime,
+              style: TextStyle(
+                fontSize: 12,
+                color: Color(0xFF616161),
+              ),
+            ),
+            Text(
+              subtitle,
+              style: TextStyle(
+                fontSize: 12,
+                color: Color(0xFF616161),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                if (isElevatedButton)
+                  ElevatedButton(
+                    onPressed: onPressed,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF675DC2),
+                    ),
+                    child: Text(
+                      buttonText,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                if (!isElevatedButton)
+                  TextButton(
+                    onPressed: onPressed,
+                    child: Text(
+                      buttonText,
+                      style: TextStyle(
+                        color: Color(0xFF675DC2),
+                      ),
+                    ),
+                  ),
+              ],
             ),
           ],
         ),
