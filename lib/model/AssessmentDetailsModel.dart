4ee062c0b901ec8fec896796a8ccd02f167b8d4c
Othello Casugod <ocasugod@kryteriononline.com>
2024-04-10 23:16:29 +0800

    Update AssessmentDetailsModel
    Update AssessmentDetailsModel

--- lib/model/AssessmentDetailsModel.dart
+++ lib/model/AssessmentDetailsModel.dart
@@ -7,12 +7,16 @@ class AssessmentDetailsModel {
   final String examTitle;
   final String examSched;
   final String examType;
+  final String registrationProgress;
+  final String? passFailStatus;
 
   AssessmentDetailsModel({
     required this.regId,
     required this.examTitle,
     required this.examSched,
     required this.examType,
+    required this.registrationProgress,
+    required this.passFailStatus,
   });
 
   factory AssessmentDetailsModel.fromJson(Map<String, dynamic> json) {
@@ -20,6 +24,8 @@ class AssessmentDetailsModel {
         regId: json['regId'],
         examTitle: json['productName'],
         examSched: json['scheduledDate'],
-        examType: json['examType']);
+        examType: json['examType'],
+        registrationProgress: json['registrationProgress'],
+        passFailStatus: json['passFailStatus']);
   }
 }
